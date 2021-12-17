a7c272333d5845d4d048b908a440408b50a9eefb
daluclemas <chidaluanukam@gmail.com>
2021-12-16 21:52:53 -0800

    finished landing page

--- src/components/logistics and footer/footer/Footer.jsx
+++ src/components/logistics and footer/footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className='footerContainer'>
+
+           <div className="footerContent">
+               <h3 className="footerHeaderText">
+                   resource edge
+               </h3>
+               
+               <p className="footerParagraph">
+                  throw paperwork into the trash 
+               </p>
+           </div>
+
+
+           <div className="footerContent">
+               <ul className="socialMedias">
+                   <li className="socialMediaLogo">
+                        <a href=""><i className="fab fa-facebook"></i></a>
+                   </li>
+
+                   <li className="socialMediaLogo">
+
+                        <a href=""><i className="fab fa-twitter"></i></a>
+
+                   </li>
+
+                   <li className="socialMediaLogo">
+
+                    <a href=""><i className="fab fa-linkedin"></i></a>
+
+                   </li>
+
+                   <li className="socialMediaLogo">
+                    <a href=""><i className="fab fa-instagram"></i></a>
+                   </li>
+               </ul>
+
+               <p className="footerParagraph">
+                   copyright &copy; <span>genesys devStudio. all</span> rights reserved.
+               </p>
+           </div>
         </div>
     )
 }
